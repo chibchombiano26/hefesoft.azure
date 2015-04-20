3813f0ebb21e6518486e7f3a86ddd056e460a664
chibchombiano@outlook.com <chibchombiano@outlook.com>
2015-04-20 02:04:44 -0500

    20 de abril

--- Azure/push.js
+++ Azure/push.js
@@ -1,138 +1,64 @@
 angular.module('starter')
-.factory('pushFactory', ['$http','urlServicioFactory' , '$q', '$rootScope','$cordovaPush', 
-  function ($http, urlServicioFactory, $q, $rootScope, $cordovaPush) {
+.controller('prestadorFechasCtrl', 
+    ['$scope', 'dataTableStorageFactory', '$ionicLoading', 'users', '$state','varsFactoryService', '$stateParams', '$cordovaDatePicker', 'pushFactory',
+	function ($scope, dataTableStorageFactory, $ionicLoading, users, $state, varsFactoryService, $stateParams, $cordovaDatePicker, pushFactory) {
+	$scope.shouldShowDelete = false;
+    $scope.shouldShowReorder = false;
+    $scope.listCanSwipe = true
+    var prestador = $stateParams.prestadorId;
 	
-	var urlBase = urlServicioFactory.getUrlBase();
-    var dataFactory = {};
+    $scope.listado =[];
+    $scope.datosCita = {};
 
-    var androidConfig = {
-        "senderID": "505952414500",
-    };
+    var options = {
+        date: new Date(),
+        mode: 'date', // or 'time'
+        minDate: new Date(),
+        allowOldDates: true,
+        allowFutureDates: false,
+        doneButtonLabel: 'Aceptar',
+        doneButtonColor: '#F2F3F4',
+        cancelButtonLabel: 'CANCELAR',
+        cancelButtonColor: '#000000'
+      };
 
-
-    $rootScope.$on('$cordovaPush:notificationReceived', function(event, notification) {
-      switch(notification.event) {
-        case 'registered':
-          if (notification.regid.length > 0 ) {
-            console.log('registration ID = ' + notification.regid);            
-          }
-          break;
-
-        case 'message':
-          // this is the actual push notification. its format depends on the data model from the push server
-          alert('message = ' + notification.message + ' msgCount = ' + notification.msgcnt);
-          break;
-
-        case 'error':
-          alert('GCM error = ' + notification.msg);
-          break;
-
-        default:
-          alert('An unknown GCM event has occurred');
-          break;
-      }
-    });
-
-    dataFactory.registerAndroid = function(){
+    $scope.$on('$ionicView.afterEnter', function(){
+        
         var deviceInformation = ionic.Platform.device();
+        var isWebView = ionic.Platform.isWebView();
+        var isIPad = ionic.Platform.isIPad();
+        var isIOS = ionic.Platform.isIOS();
         var isAndroid = ionic.Platform.isAndroid();
+        var isWindowsPhone = ionic.Platform.isWindowsPhone();
 
-      if(isAndroid){
-            $cordovaPush.register(androidConfig).then(function(result) {
-                console.log(result);
-                var key = result;
-                dataFactory.register(key);
-            }, function(err) {
-                console.log(err);
-            })
-          }
-      else{
+        var currentPlatform = ionic.Platform.platform();
+        var currentPlatformVersion = ionic.Platform.version();
 
-        //Key de pruebas
-        var key = "APA91bFFa2kqzL9AE8utHBuoE4B-AtnQZKQuRPIdSP50PbeQEbjTsLUC4ZCyLOnKc7A1jYg91TuQ7_29PUqZjh5H9lyqT0-pmcDQE4JTWNLHlEdCMXyV3nPUCLQMdnGs22fEKSTO5ht9I5paXjCabIxT4veR55F9bfx2d4U7GRKaNRn3q212m2Q";
-        dataFactory.register(key);
-      }
-    }
-
-    dataFactory.register = function(key){
-    	var deferred = $q.defer();
-    	var key = key;
-    	$http.post(urlBase + "api/register", {key : key}).success(function (data) {                
-                updateRegister(data, deferred, key);
-            })
-            .error(function (error) {
-                
+        if(isAndroid || isIOS || isIPad){
+             $cordovaDatePicker.show(options).then(function(date){
+                $scope.datosCita.fecha = date;
+                console.log(date);
             });
+        }
 
-       return deferred.promise;
+    });
+    
+
+    $scope.solicitarCita = function(){
+        var usuario = users.getCurrentUser();
+        var item = varsFactoryService.prestadorSeleccionado();
+        var platformPush = pushFactory.getPlatform();
+
+        var data = {
+            prestador : item.username,
+            platform : platformPush,
+            PartitionKey : item.username,
+            RowKey: usuario.username,
+            solicitadaPor : usuario.username,
+            nombreTabla: 'TmCitas',
+            fecha : $scope.datosCita.fecha
+        }
+
+        dataTableStorageFactory.saveStorage(data);
     }
-
-    //Un tag o un nombre
-    dataFactory.enviarMensaje = function(enviarA, mensaje){
-      var platformVersion = getPlatform();
-    	var item = { platform : platformVersion, to_tag : enviarA, mensaje : mensaje};
-    	$http.post(urlBase + "api/notifications", item)
-    	.success(function (data) {                
-        	console.log(data);        
-        })
-        .error(function (error) {
-          	console.log(error);      
-        });
-    }
-
-	return  dataFactory;
-
-	function updateRegister(data, promise, key){
-		var deferred = promise;
-		var tag = "Pacientes, Odontologia";
-		var platform = getPlatform();
-
-		//Falta habilitar para m
-		var item ={idhubazure : data, tag : tag, platform: platform, key : key};
-
-		var req = {
-                method: 'PUT', 
-                url: urlBase + "api/register",
-                data: item,
-                headers: {'Content-Type': 'application/x-www-form-urlencoded',
-                'Content-Type': 'application/json'
-            	}
-            }
-            $http(req).success(function(data) { 
-                console.log(data);               
-                deferred.resolve(data);
-                //Prueba
-                //dataFactory.enviarMensaje("futbolito152@gmail.com", "futbolito152@gmail.com");
-            }).error(function(data) { 
-                console.log(data);
-                deferred.reject(data);                 
-            });
-	}
-
-  dataFactory.getPlatform = function(){
-      return getPlatform();    
-  }
-
-
-
-	function getPlatform(){
-    	var deviceInformation = ionic.Platform.device();
-      var isAndroid = ionic.Platform.isAndroid();
-      var isWebView = ionic.Platform.isWebView();
-      var isIPad = ionic.Platform.isIPad();
-      var isIOS = ionic.Platform.isIOS();
-      var isAndroid = ionic.Platform.isAndroid();
-      var isWindowsPhone = ionic.Platform.isWindowsPhone();
-
-      var currentPlatform = ionic.Platform.platform();
-      var currentPlatformVersion = ionic.Platform.version();
-
-      if(isAndroid){
-  			return  "gcm";
-	  }
-      else{
-      	return "gcm";
-      }
-	}
-
 }])
